49b94828858d49e4a28a513eee5ddb5abe13f515
Sinji <sinzi031012@naver.com>
2024-05-12 21:15:26 +0900

    init: theme 색상명 지정

---
+++ src/styles/theme.ts
@@ -0,0 +1,38 @@
+const colors = {
+  b01: "#000000",
+  b02: "#0D0D0D",
+  b03: "#161C1C",
+  b04: "#231F20",
+  b05: "#2C3030",
+  b06: "#A6A6A6",
+  b07: "#A1A6A6",
+  b08: "#768585",
+  b09: "#7C8484",
+
+  w01: "#FFFFFF",
+
+  g01: "#F9FAFC",
+  g02: "#F0F5F5",
+  g03: "#E4E8E8",
+  g04: "#DFDFDF",
+  g05: "#E1E1E1",
+
+  o01: "#FFF5DA",
+  o02: "#FF7900",
+  o03: "#FFF1E1",
+
+  gg01: "#00CD80",
+  gg02: "#01BA61",
+
+  b01: "#EDFBFB",
+  b02: "#E7F3FF",
+  b03: "#1ECAD3",
+  b04: "#05BCC6",
+  b05: "#1289F6",
+};
+
+const theme = {
+  colors,
+};
+
+export default theme;
