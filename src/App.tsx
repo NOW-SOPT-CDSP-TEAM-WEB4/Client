df45b034e515d8673b00d5cd209b9fa6d0e593a2
jungwoo3490 <jungwoo3490@naver.com>
2024-05-14 14:38:02 +0900

    fix: GlobalStyle 확장자 변경 및 미적용 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
 import { router } from "./Router";
+import GlobalStyle from "./styles/GlobalStyle";
 import theme from "./styles/theme";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <RouterProvider router={router} />
       </ThemeProvider>
     </>
