87cf067f3c35bba044b109c78d86bc9ff1329db9
Sinji <sinzi031012@naver.com>
2024-05-18 00:55:30 +0900

    feat: 사이드바 토글 제외한 UI 전체 구현

--- src/components/Community/CommunitySidebar.tsx
+++ src/components/Community/CommunitySidebar.tsx
@@ -2,15 +2,20 @@ import { useState } from "react";
 
 import { styled } from "styled-components";
 
-import { ImgHouse, ImgWorkshop, ImgProject, ImgCommuity, ImgContents } from "../../assets";
+import { ImgHouse, ImgWorkshop, ImgProject, ImgCommuity, ImgContents, ImgDesign, ImgOpen } from "../../assets";
 
 function CommunitySidebar() {
-  const category = [
-    { text: "홈", element: <ImgHouse /> },
-    { text: "워크숍&세미나", element: <ImgWorkshop /> },
-    { text: "프로젝트&챌린지", element: <ImgProject /> },
-    { text: "커뮤니티", element: <ImgCommuity /> },
-    { text: "콘텐츠", element: <ImgContents /> },
+  const navCategory = [
+    { content: "홈", element: <ImgHouse /> },
+    { content: "워크숍&세미나", element: <ImgWorkshop /> },
+    { content: "프로젝트&챌린지", element: <ImgProject /> },
+    { content: "커뮤니티", element: <ImgCommuity /> },
+    { content: "콘텐츠", element: <ImgContents /> },
+  ];
+
+  const outLinkCategory = [
+    { content: "디자이너 생존 테스트", element: <ImgDesign /> },
+    { content: "디자이너 오픈 채팅방", element: <ImgOpen /> },
   ];
 
   const [btnActive, setBtnActive] = useState(0);
@@ -22,21 +27,31 @@ function CommunitySidebar() {
   return (
     <CommunutySidebarWrapper>
       <SidebarBtnContainer>
-        {category.map((item, idx) => {
+        {navCategory.map((item, idx) => {
           return (
             <SidebarBtn
               type="button"
-              key={item.text}
+              key={item.content}
               value={idx}
               className={"btn" + (idx == btnActive ? " active" : "")}
               // 현재는 홈에 바탕색 고정. 클릭마다 바탕색 이벤트를 주고 싶을 경우 0 >> idx로 변경
               onClick={() => toggleActive(0)}>
-              <ImgContainer>{item.element}</ImgContainer>
-              {item.text}
+              <NavImg>{item.element}</NavImg>
+              {item.content}
             </SidebarBtn>
           );
         })}
       </SidebarBtnContainer>
+      <OutLinkBtnContainer>
+        {outLinkCategory.map((item) => {
+          return (
+            <OutLinkBtn type="button" key={item.content}>
+              <OutLinkImg>{item.element}</OutLinkImg>
+              {item.content}
+            </OutLinkBtn>
+          );
+        })}
+      </OutLinkBtnContainer>
     </CommunutySidebarWrapper>
   );
 }
@@ -75,8 +90,37 @@ const SidebarBtn = styled.button`
   }
 `;
 
-const ImgContainer = styled.div`
+const NavImg = styled.div`
   display: flex;
 
   margin-right: 1.2rem;
+`;
+
+const OutLinkBtnContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  align-self: stretch;
+
+  margin-top: 2.4rem;
+  padding: 0 1.6rem;
+
+  gap: 1.2rem 0;
+`;
+
+const OutLinkBtn = styled.button`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
+
+  padding: 1.6rem 3rem 1.6rem 2rem;
+
+  ${({ theme }) => theme.fonts.SemiBold14_17};
+`;
+
+const OutLinkImg = styled.div`
+  display: flex;
+
+  margin-right: auto;
 `;
--- src/pages/Community/Community.tsx
+++ src/pages/Community/Community.tsx
@@ -1,7 +1,17 @@
+import { styled } from "styled-components";
+
 import CommunitySidebar from "../../components/Community/CommunitySidebar";
 
 function Community() {
-  return <CommunitySidebar></CommunitySidebar>;
+  return (
+    <CommunityWrapper>
+      <CommunitySidebar />
+    </CommunityWrapper>
+  );
 }
 
 export default Community;
+
+const CommunityWrapper = styled.div`
+  width: 100%;
+`;
