182419af68e705d55544e6af79487f024d529ded
Sinji <sinzi031012@naver.com>
2024-05-17 18:59:26 +0900

    chore: svg 세팅

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,10 @@
+import ImgCommuity from "./img/img_community.svg?react";
+import ImgContents from "./img/img_contents.svg?react";
+import ImgDesign from "./img/img_design.svg?react";
+import ImgHouse from "./img/img_house.svg?react";
+import ImgOpen from "./img/img_open.svg?react";
+import ImgProject from "./img/img_project.svg?react";
+import ImgWorkshop from "./img/img_workshop.svg?react";
 import IcCheckWhite from "./svg/ic_check_white.svg?react";
 import IcLogoSim from "./svg/ic_logo_sim.svg?react";
 import IcLoud from "./svg/ic_loud.svg?react";
@@ -8,4 +15,21 @@ import IcNew from "./svg/icons_New.svg?react";
 import IcSearch from "./svg/icons_Search.svg?react";
 import Logo from "./svg/logo.svg?react";
 
-export { Logo, IcCheckWhite, IcNextButton, IcLogoSim, IcLoud, IcSym, IcTopBar, IcSearch, IcNew };
+export {
+  Logo,
+  IcCheckWhite,
+  IcNextButton,
+  IcLogoSim,
+  IcLoud,
+  IcSym,
+  IcTopBar,
+  IcSearch,
+  IcNew,
+  ImgCommuity,
+  ImgContents,
+  ImgDesign,
+  ImgHouse,
+  ImgOpen,
+  ImgProject,
+  ImgWorkshop,
+};
