462baa55a4cdb958283ec3598bb71125f6653290
jungwoo3490 <jungwoo3490@naver.com>
2024-05-18 00:23:58 +0900

    feat: 채용 페이지 상단 Banner 컨테이너 구현

--- src/pages/Recruit/Recruit.tsx
+++ src/pages/Recruit/Recruit.tsx
@@ -1,5 +1,17 @@
+import { styled } from "styled-components";
+
+import BannerContainer from "../../components/Recruit/BannerContainer/BannerContainer";
+
 function Recruit() {
-  return <div>Recruit</div>;
+  return (
+    <RecruitWrapper>
+      <BannerContainer />
+    </RecruitWrapper>
+  );
 }
 
 export default Recruit;
+
+const RecruitWrapper = styled.div`
+  width: 100%;
+`;
