8a4a4adff60aec9f5067744cbf04660385b17ebe
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-05-18 14:32:40 +0900

    chore: svg 파일 다운로드

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,8 @@
 import IcCheckWhite from "./svg/ic_check_white.svg?react";
+import IcCsFour from "./svg/ic_cs_four.svg?react";
+import IcCsOne from "./svg/ic_cs_one.svg?react";
+import IcCsThree from "./svg/ic_cs_three.svg?react";
+import IcCsTwo from "./svg/ic_cs_two.svg?react";
 import IcLogoSim from "./svg/ic_logo_sim.svg?react";
 import IcLoud from "./svg/ic_loud.svg?react";
 import IcNextButton from "./svg/ic_next_button.svg?react";
@@ -8,4 +12,18 @@ import IcNew from "./svg/icons_New.svg?react";
 import IcSearch from "./svg/icons_Search.svg?react";
 import Logo from "./svg/logo.svg?react";
 
-export { Logo, IcCheckWhite, IcNextButton, IcLogoSim, IcLoud, IcSym, IcTopBar, IcSearch, IcNew };
+export {
+  Logo,
+  IcCheckWhite,
+  IcNextButton,
+  IcLogoSim,
+  IcLoud,
+  IcSym,
+  IcTopBar,
+  IcSearch,
+  IcNew,
+  IcCsOne,
+  IcCsTwo,
+  IcCsThree,
+  IcCsFour,
+};
---
+++ src/components/Home/Carousel.tsx
@@ -0,0 +1,5 @@
+function Carousel() {
+  return <div>dd</div>;
+}
+
+export default Carousel;
