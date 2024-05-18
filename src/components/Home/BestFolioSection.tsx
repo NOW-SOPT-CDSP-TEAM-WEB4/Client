882308081ea10ce0b82f6d19d68d2eb0378bdddd
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-05-19 02:50:29 +0900

    feat: ChipList 불러오기

--- src/components/Home/BestFolioSection.tsx
+++ src/components/Home/BestFolioSection.tsx
@@ -1,12 +1,16 @@
 import { styled } from "styled-components";
 
 import { IcFrame } from "../../assets/index";
+import ChipsList from "../common/ChipList/ChipsList";
+import { chipsTextList } from "../common/ChipList/constantEx";
 
 function BestFolioSection() {
   return (
     <BestFolioSectionWrapper>
       <BestFolioSectionTitle>국내 최고의 포트폴리오를 발견해보세요.</BestFolioSectionTitle>
-      <BestFolioChipContainer></BestFolioChipContainer>
+      <BestFolioChipContainer>
+        <ChipsList chipsTextList={chipsTextList} />
+      </BestFolioChipContainer>
       <ToggleBtnSectionContainer>
         <SortingMethod>정렬방식</SortingMethod>
         <NotefolioPick>노트폴리오 픽</NotefolioPick>
@@ -27,16 +31,19 @@ const BestFolioSectionWrapper = styled.section`
 const BestFolioSectionTitle = styled.h2`
   ${({ theme }) => theme.fonts.Bold28_36};
   color: ${({ theme }) => theme.colors.b03};
+  letter-spacing: -0.05rem;
 `;
 
 const BestFolioChipContainer = styled.section`
-  display: flex;
+  width: 100%;
+  height: 6.7rem;
+  margin-top: 1rem;
 `;
 
 const ToggleBtnSectionContainer = styled.section`
   display: flex;
   align-items: center;
-  margin-top: 3.45rem;
+  margin-top: 3.1rem;
   gap: 0.8rem;
   align-self: stretch;
 `;
@@ -49,6 +56,7 @@ const SortingMethod = styled.p`
 const NotefolioPick = styled.p`
   ${({ theme }) => theme.fonts.SemiBold15_24};
   color: ${({ theme }) => theme.colors.b03};
+  letter-spacing: -0.025rem;
 `;
 
 const IcFrameIcon = styled(IcFrame)`
