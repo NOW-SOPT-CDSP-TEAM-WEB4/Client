e61c637e00c28d150d0ca29153dc8032005452e2
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-05-19 03:43:09 +0900

    chore: homeConstants 파일 생성

---
+++ src/constants/Home/homeConstants.tsx
@@ -0,0 +1,44 @@
+import { IcMog, IcKinder, IcMolly, IcStudio, IcFloggy, IcView, IcGreyHeart } from "../../assets/index";
+
+export const bestfolioHomeList = [
+  {
+    id: 0,
+    imgSrc: "/src/assets/img/img_bestfolio_home_1.png",
+    company: "모그 스튜디오",
+    companyLogo: <IcMog />,
+    viewLogo: <IcView />,
+    greyHeartLogo: <IcGreyHeart />,
+  },
+  {
+    id: 1,
+    imgSrc: "/src/assets/img/img_bestfolio_home_2.png",
+    company: "킨더가든",
+    companyLogo: <IcKinder />,
+    viewLogo: <IcView />,
+    greyHeartLogo: <IcGreyHeart />,
+  },
+  {
+    id: 2,
+    imgSrc: "/src/assets/img/img_bestfolio_home_3.png",
+    company: "몰리(Molly)",
+    companyLogo: <IcMolly />,
+    viewLogo: <IcView />,
+    greyHeartLogo: <IcGreyHeart />,
+  },
+  {
+    id: 3,
+    imgSrc: "/src/assets/img/img_bestfolio_home_4.png",
+    company: "스튜디오 디워크",
+    companyLogo: <IcStudio />,
+    viewLogo: <IcView />,
+    greyHeartLogo: <IcGreyHeart />,
+  },
+  {
+    id: 4,
+    imgSrc: "/src/assets/img/img_bestfolio_home_5.png",
+    company: "Floggy",
+    companyLogo: <IcFloggy />,
+    viewLogo: <IcView />,
+    greyHeartLogo: <IcGreyHeart />,
+  },
+];
