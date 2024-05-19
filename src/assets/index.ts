89e2ffb05467f457487945c6274ecaccc4eccfeb
jungwoo3490 <jungwoo3490@naver.com>
2024-05-19 23:32:35 +0900

    chore: 필요 asset 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,6 @@
+import IcArrowDown from "./svg/ic_arrow_down.svg?react";
 import IcCheckWhite from "./svg/ic_check_white.svg?react";
+import IcFilter from "./svg/ic_filter.svg?react";
 import IcLogoSim from "./svg/ic_logo_sim.svg?react";
 import IcLoud from "./svg/ic_loud.svg?react";
 import IcNextButton from "./svg/ic_next_button.svg?react";
@@ -8,4 +10,4 @@ import IcNew from "./svg/icons_New.svg?react";
 import IcSearch from "./svg/icons_Search.svg?react";
 import Logo from "./svg/logo.svg?react";
 
-export { Logo, IcCheckWhite, IcNextButton, IcLogoSim, IcLoud, IcSym, IcTopBar, IcSearch, IcNew };
+export { IcArrowDown, IcCheckWhite, IcFilter, IcLogoSim, IcLoud, IcNew, IcNextButton, IcSearch, IcSym, IcTopBar, Logo };
