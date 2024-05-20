9eefbc83f7d89850a5de3a86319df1be7e581da8
jungwoo3490 <jungwoo3490@naver.com>
2024-05-20 16:45:42 +0900

    chore: 필요 asset 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,17 @@
 import ImgRecruitBanner from "./img/img_recruit_banner.svg?react";
 import IcCheckWhite from "./svg/ic_check_white.svg?react";
 import IcClock from "./svg/ic_clock.svg?react";
+import IcDesignerRecruit1 from "./svg/ic_designer_recruit_1.svg?react";
+import IcDesignerRecruit10 from "./svg/ic_designer_recruit_10.svg?react";
+import IcDesignerRecruit11 from "./svg/ic_designer_recruit_11.svg?react";
+import IcDesignerRecruit2 from "./svg/ic_designer_recruit_2.svg?react";
+import IcDesignerRecruit3 from "./svg/ic_designer_recruit_3.svg?react";
+import IcDesignerRecruit4 from "./svg/ic_designer_recruit_4.svg?react";
+import IcDesignerRecruit5 from "./svg/ic_designer_recruit_5.svg?react";
+import IcDesignerRecruit6 from "./svg/ic_designer_recruit_6.svg?react";
+import IcDesignerRecruit7 from "./svg/ic_designer_recruit_7.svg?react";
+import IcDesignerRecruit8 from "./svg/ic_designer_recruit_8.svg?react";
+import IcDesignerRecruit9 from "./svg/ic_designer_recruit_9.svg?react";
 import IcLogoDailyMorning from "./svg/ic_logo_daily_morning.svg?react";
 import IcLogoSim from "./svg/ic_logo_sim.svg?react";
 import IcLogoStudioEdink from "./svg/ic_logo_studio_edink.svg?react";
@@ -16,6 +27,17 @@ import Logo from "./svg/logo.svg?react";
 export {
   IcCheckWhite,
   IcClock,
+  IcDesignerRecruit1,
+  IcDesignerRecruit10,
+  IcDesignerRecruit11,
+  IcDesignerRecruit2,
+  IcDesignerRecruit3,
+  IcDesignerRecruit4,
+  IcDesignerRecruit5,
+  IcDesignerRecruit6,
+  IcDesignerRecruit7,
+  IcDesignerRecruit8,
+  IcDesignerRecruit9,
   IcLogoDailyMorning,
   IcLogoSim,
   IcLogoStudioEdink,
