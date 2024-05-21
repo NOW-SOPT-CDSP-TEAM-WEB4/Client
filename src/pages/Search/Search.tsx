fa928db55ff3a8e16b42e8795d49227249c134e5
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-05-21 14:23:47 +0900

    style: 하트 svg 변경

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,19 +1,5 @@
-import { styled } from "styled-components";
-
-import SearchInputSection from "../../components/Search/SearchInputSection";
-import SearchResultSection from "../../components/Search/SearchResultSection";
-
 function Search() {
-  return (
-    <SearchWrapper>
-      <SearchInputSection />
-      <SearchResultSection />
-    </SearchWrapper>
-  );
+  return <div>Search</div>;
 }
 
 export default Search;
-
-const SearchWrapper = styled.div`
-  width: 100%;
-`;
