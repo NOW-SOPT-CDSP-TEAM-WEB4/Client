9f9c885e7087235259c19041743158967749d847
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-05-21 15:40:52 +0900

    feat: DesignerRecruitSection 구현

--- src/components/Home/DesignerRecruitSection/DesignerRecruitSection.tsx
+++ src/components/Home/DesignerRecruitSection/DesignerRecruitSection.tsx
@@ -1,7 +1,12 @@
 import { styled } from "styled-components";
 
 function DesignerRecruitSection() {
-  return <DesignerRecruitSectionWrapper></DesignerRecruitSectionWrapper>;
+  return (
+    <DesignerRecruitSectionWrapper>
+      <DesignerRecruitSectionTitle>디자이너 채용 정보는 노트폴리오에서!</DesignerRecruitSectionTitle>
+      <DesignerRecruitItemContainer></DesignerRecruitItemContainer>
+    </DesignerRecruitSectionWrapper>
+  );
 }
 
 export default DesignerRecruitSection;
@@ -9,7 +14,20 @@ export default DesignerRecruitSection;
 const DesignerRecruitSectionWrapper = styled.section`
   display: flex;
   width: 100%;
-  padding: 3.5rem 5.9rem 4.2rem 6rem;
-  gap: 2.6rem;
+  padding: 0 calc((100vw - 124.4rem) / 2);
   background-color: ${({ theme }) => theme.colors.b03};
+`;
+
+const DesignerRecruitSectionTitle = styled.h2`
+  ${({ theme }) => theme.fonts.Bold28_36};
+  color: ${({ theme }) => theme.colors.w01};
+  margin-top: 3.5rem;
+`;
+
+const DesignerRecruitItemContainer = styled.article`
+  display: flex;
+  column-gap: 2.2rem;
+
+  width: 100%;
+  margin-top: 2.6rem;
 `;
