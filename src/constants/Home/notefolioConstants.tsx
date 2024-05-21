a9a62aedbae98db527483bef30edd8f9c9b49e1a
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-05-21 22:32:24 +0900

    feat: notefolioConstants 생성

---
+++ src/constants/Home/notefolioConstants.tsx
@@ -0,0 +1,40 @@
+import { IcJaein, IcKush, IcMozza, IcStudioKio } from "../../assets";
+
+export const notefolioRecommendList = [
+  {
+    id: 0,
+    imgSrc: ["/src/assets/img/img_kio_1.png", "/src/assets/img/img_kio_2.png", "/src/assets/img/img_kio_3.png"],
+    companyLogo: <IcStudioKio />,
+    company: "StudioKio",
+  },
+  {
+    id: 1,
+    imgSrc: [
+      "/src/assets/img/img_pindit_1.png",
+      "/src/assets/img/img_pindit_2.png",
+      "/src/assets/img/img_pindit_3.png",
+    ],
+    companyLogo: <IcStudioKio />,
+    company: "StudioKio",
+    content: "그래픽 디자인 | 캐릭터 ...",
+  },
+  {
+    id: 2,
+    imgSrc: ["/src/assets/img/img_KUSH_1.png", "/src/assets/img/img_KUSH_2.png", "/src/assets/img/img_KUSH_3.png"],
+    companyLogo: <IcKush />,
+    company: "KUSH",
+  },
+  {
+    id: 3,
+    imgSrc: ["/src/assets/img/img_Mozza_1.png", "/src/assets/img/img_Mozza_2.png", "/src/assets/img/img_Mozza_3.png"],
+    companyLogo: <IcMozza />,
+    company: "Mozza",
+    content: "그래픽 디자인 | ...",
+  },
+  {
+    id: 4,
+    imgSrc: ["/src/assets/img/img_jaein_1.png", "/src/assets/img/img_jaein_2.png", "/src/assets/img/img_jaein_3.png"],
+    companyLogo: <IcJaein />,
+    company: "JAE IN",
+  },
+];
