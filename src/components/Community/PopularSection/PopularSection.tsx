88b2d6f53efb2a86461e0b95bccd98b296efb5c3
Sinji <sinzi031012@naver.com>
2024-05-21 21:54:51 +0900

    chore: popular section에 들어갈 이미지 추가

---
+++ src/components/Community/PopularSection/PopularSection.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+function PopularSection() {
+  return <PopularSectionWrapper></PopularSectionWrapper>;
+}
+
+export default PopularSection;
+
+const PopularSectionWrapper = styled.section`
+  display: flex;
+`;
