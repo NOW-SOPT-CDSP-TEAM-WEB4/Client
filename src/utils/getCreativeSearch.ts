4eb49cfac6d92971be8db08a76b4639b90cf50b5
jungwoo3490 <jungwoo3490@naver.com>
2024-05-23 16:07:43 +0900

    chore: 폴더 이동으로 인한 기존 파일 삭제

--- src/utils/getCreativeSearch.ts
+++
@@ -1,6 +0,0 @@
-import { api } from "../libs/api";
-
-export const getCreativeSearch = async (keyword: string) => {
-  const res = await api.get(`/creatives/search?word=${keyword}`);
-  return res.data.data.creatives;
-};
