941668b4bdca74ceba0c508b8e738a0a91e2586d
jungwoo3490 <jungwoo3490@naver.com>
2024-05-24 17:06:22 +0900

    fix: Page 날아간 거 복구

--- src/pages/Recruit/Recruit.tsx
+++ src/pages/Recruit/Recruit.tsx
@@ -1,5 +1,27 @@
+import { styled } from "styled-components";
+
+import ActiveRecruitSection from "../../components/Recruit/ActiveRecruitSection";
+import BannerSection from "../../components/Recruit/BannerSection";
+import DesignerRecruitSection from "../../components/Recruit/DesignerRecruitSection";
+
 function Recruit() {
-  return <div>Recruit</div>;
+  return (
+    <RecruitWrapper>
+      <BannerSection />
+      <RecruitBodyWrapper>
+        <ActiveRecruitSection />
+        <DesignerRecruitSection />
+      </RecruitBodyWrapper>
+    </RecruitWrapper>
+  );
 }
 
 export default Recruit;
+
+const RecruitWrapper = styled.div`
+  width: 100%;
+`;
+
+const RecruitBodyWrapper = styled.main`
+  padding: 0 calc((100vw - 130.2rem) / 2);
+`;
--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,5 +1,19 @@
+import { styled } from "styled-components";
+
+import SearchInputSection from "../../components/Search/SearchInputSection";
+import SearchResultSection from "../../components/Search/SearchResultSection";
+
 function Search() {
-  return <div>Search</div>;
+  return (
+    <SearchWrapper>
+      <SearchInputSection />
+      <SearchResultSection />
+    </SearchWrapper>
+  );
 }
 
 export default Search;
+
+const SearchWrapper = styled.div`
+  width: 100%;
+`;
